0269cd450b86dd1185adee54c9c26a955e187ad2
sahar085 <37782726+shfhm@users.noreply.github.com>
2018-05-20 00:52:16 -0400

    Add files via upload

---
+++ Check Power of 2.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[10]:
+
+
+import math
+def sqrtt(x):
+    i=math.sqrt(x)
+    if i**2==x:
+        print('the input is power 2 of %s' %(i) )
+    else:
+        print('it\'s not the power 2')
+
