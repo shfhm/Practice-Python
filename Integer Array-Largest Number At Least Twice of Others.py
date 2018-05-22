e2cb03fa0070c077c957a747ff4ef907d0a88385
Sahar <37782726+shfhm@users.noreply.github.com>
2018-05-22 13:37:39 -0400

    Update Integer Array-Largest Number At Least Twice of Others.py

--- Integer Array-Largest Number At Least Twice of Others.py
+++ Integer Array-Largest Number At Least Twice of Others.py
@@ -1,22 +1,23 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 __author__ = 'Sahar'
 
 
 def max(a):
     x=0
-    for x in range(0,2):
+    for x in range(0,len(a)-1):
         if a[x+1] > a[x]:
-            max=a[x+1]        
+            max=a[x+1] 
+        else:
+            max=a[x]
     print(max)
+    index=0
     for i in a:
-        if i*2 == max:
-            return True
-    return False
+        mul2=i*2
+        if max == mul2:
+            print(index)
+            break
+        index+=1
+    if index > len(a)-1:
+        return -1
     
   
 
