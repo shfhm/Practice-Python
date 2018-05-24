31cce919c1ae8ebf8fb6076a602a0a0abdde4bda
Sahar <37782726+shfhm@users.noreply.github.com>
2018-05-24 12:58:18 -0400

    Add files via upload

---
+++ longestCommonPrefix.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+def strs(str):
+    if not str:
+        return""
+    key=str[0] 
+    len_key=len(key)
+    for i in str:
+        #len_key=len(key)
+        #while i <= len_key:
+        while key != i[0:len_key]:
+            len_key-=1
+            key=key[0:len_key]        
+    return key
+
