3792bd4411260adaa350fce3cc72597c0296e10c
Sahar <37782726+shfhm@users.noreply.github.com>
2018-05-24 23:40:41 -0400

    Add files via upload

---
+++ Remove Element withOUT repeat.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+__author__ = 'Sahar'
+list=[]
+for j in a:
+    if a.count(j) == 1:
+        list.append(j)
+    if a.count(j) > 1:
+        if j != target:
+            if j not in list:  
+                list.append(j)   
+print(list)
+
