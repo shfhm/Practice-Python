10a1f564ad7abcaf460845a84d96e03b773f9959
Sahar <37782726+shfhm@users.noreply.github.com>
2018-06-05 00:43:11 -0400

    Add files via upload

---
+++ Minimum depth of a binary tree-recursion.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+#minimum depth of a binary tree-recursion
+
+class treenode(object):
+    def __init__(self, root):
+        self.val=root
+        self.left= None
+        self.right = None
+class solution(object):
+    def mindepth(self, root):
+        if root == None:
+            return 0
+        if((root.left == None) and (root.right == None)):
+            return 1
+        return 1+min(self.mindepth(root.left), self.mindepth(root.right))
+
+s=solution()
+root=treenode(1)    
+root.left=treenode(2)
+root.right=treenode(5)
+root.left.left=treenode(3)
+root.left.right=treenode(4)
+print(s.mindepth(root))
+
