e2c9f21c9e768512056c39076237d1f1541af2eb
Sahar <37782726+shfhm@users.noreply.github.com>
2018-06-05 02:07:45 -0400

    Add files via upload

---
+++ Minimun Depth of a Binary Tree-Level Order.py
@@ -0,0 +1,43 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#Minimun Depth of a Binary Tree- Level Order
+#The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+class treenode(object):
+    def __init__(self, root):
+        self.val=root
+        self.left= None
+        self.right= None
+
+class solution(object):
+    def mindepth(self, root):
+        if (root == None):
+            return 0
+        q1 = []
+        q1.append(root)
+        depth=1
+        while q1:
+            q2=[]
+            while q1:
+                root=q1.pop()
+                if (root.left):
+                    q2.append(root.left)
+                if (root.right):
+                    q2.append(root.right)
+                if ((root.left == None) and (root.right ==None)):
+                    return depth
+            q1=q2
+            depth+=1
+        return depth
+s=solution()
+root=treenode(1)
+root.left=treenode(2)
+root.left.left=treenode(3)
+root.left.left.left=treenode(4)
+root.left.left.left.left=treenode(5)
+print(s.mindepth(root))
+
