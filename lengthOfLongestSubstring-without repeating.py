7651283cccf99ed0c19cb0e6366dafb74ae801b5
Sahar <37782726+shfhm@users.noreply.github.com>
2018-06-09 03:09:41 -0400

    Add files via upload

---
+++ lengthOfLongestSubstring-without repeating.py
@@ -0,0 +1,32 @@
+
+# coding: utf-8
+
+# In[22]:
+
+
+__Author__= 'Sahar'
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        seen={}
+        start=0
+        max_len=0
+        for end in range(len(s)):
+            if s[end] not in seen:
+                seen[s[end]]=end
+                max_len=max(max_len, end-start+1)
+            else:
+                start= max(start,seen[s[end]]+1 )
+                max_len=max(max_len, end-start+1)
+                seen[s[end]]=end
+        return(max_len)
+    
+#s="jxdlnaaij"
+s="tmmzuxt"
+t=Solution()
+print(t.lengthOfLongestSubstring(s))
+
