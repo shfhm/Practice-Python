09968356d3c262dc17b850f42d1d9f9a70132347
Sahar <37782726+shfhm@users.noreply.github.com>
2018-06-16 19:13:25 -0400

    Add files via upload

---
+++ MapSum Pair.py
@@ -0,0 +1,27 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+class Mapsum:
+    def __init__(self):
+        self.map={}
+        
+    def insert(self, key, val):
+        self.map[key]=val
+        print("Null")
+        
+    def sum(self, prefix):
+        sum=0
+        for key, val in self.map.items():
+            if key.find(prefix) == 0:
+                sum+=self.map[key]    #val
+        return sum
+    
+T=Mapsum()
+T.insert("darabe",3)
+print(T.sum("da"))
+print(T.insert("dara",2))
+print(T.sum("da"))
+
