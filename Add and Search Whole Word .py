6aad01a527c9f6253d16193bd2e597981747a1f6
Sahar <37782726+shfhm@users.noreply.github.com>
2018-06-19 03:31:14 -0400

    Add files via upload

---
+++ Add and Search Whole Word .py
@@ -0,0 +1,40 @@
+
+# coding: utf-8
+
+# In[11]:
+
+
+class Trienode(object):
+    def __init__(self):
+        self.dict={}
+        self.ends=False
+        
+class AddWords(object):
+    def __init__(self):
+        self.root=Trienode()
+        
+    def insert(self, word):
+        cur = self.root
+        for char in word:
+            child=cur.dict.get(char)
+            if child is None:
+                child=Trienode()
+                cur.dict[char]=child  
+            cur=child
+        cur.ends=True
+        
+    def search(self, word):
+        cur=self.root
+        for char in word:
+            child=cur.dict.get(char)
+            if child is None:         #search whole word
+                return False
+            else:
+                cur=child
+        return cur.ends
+    
+s=AddWords()
+print(s.insert('ab'))
+print(s.search('ab'))
+print(s.search('a'))         
+
