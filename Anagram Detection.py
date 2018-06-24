97e8b116ad6b49e4510b5a535dc1a72a84814275
Sahar <37782726+shfhm@users.noreply.github.com>
2018-06-23 20:34:11 -0400

    Add files via upload

---
+++ Anagram Detection.py
@@ -0,0 +1,31 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#Aanagram detection
+#Big-O is o(n^2)
+
+"""
+the anagram problem will check to see that each character in the first
+string actually occurs in the second
+"""
+
+def sol(s1,s2):
+    l1=list(s1)
+    l2=list(s2)
+    l1.sort()
+    l2.sort()
+    index=0
+    matches = True
+    while index < len(s1) and matches:
+        if l2[index] == l1[index]:
+            index=index+1
+        else:
+            matches = False
+    return matches
+    
+print(sol('snbgf','fbsng'))
+print(sol('snbgl','fbsng'))
+
